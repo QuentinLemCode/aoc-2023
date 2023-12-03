a774750a683d11312ce040fe6f14377ee9d000af
QuentinLemCode <quentinlemcode@gmail.com>
2023-12-03 11:18:19 +0100

    day3 part 2

--- day3.rb
+++ day3.rb
@@ -52,4 +52,55 @@ def find_symbol(input, index, offset)
   end
 end
 
+def find_gear(input, index, offset)
+  min = [offset[0] - 1, 0].max
+  max = [offset[1] + 1, input[index].length - 1].min - 1
+
+  if index > 0
+    input[index - 1][min..max].each_char do |char|
+      return true if char&.match?('*')
+    end
+  end
+  input[index][min..max].each_char { |char| return true if char&.match?('*') }
+  if index < input.length - 1
+    input[index + 1][min..max].each_char do |char|
+      return true if char&.match?('*')
+    end
+  end
+end
+
+def part2(input)
+  gears = {}
+  input = input.split("\n")
+  input.each_with_index do |line, index|
+    line.scan('*') do |number|
+      offset = $~.offset(0)
+      gears["#{index}-#{offset[0]}"] = []
+    end
+  end
+
+  input.each_with_index do |line, index|
+    line.scan(/\d+/) do |number|
+      offset = $~.offset(0)
+      min = [offset[0] - 1, 0].max
+      max = [offset[1] + 1, input[index].length - 1].min - 1
+
+      for j in -1..1
+        next if index + j < 0 || index + j > input.length - 1
+        idx = index + j
+
+        input[idx][min..max].each_char.with_index do |char, i|
+          if char&.include?('*') && gears["#{idx}-#{i+min}"].nil?
+            puts 'a'
+          end
+          gears["#{idx}-#{i+min}"] << number if char&.include?('*')
+        end
+      end
+    end
+  end
+
+  gears.filter { |k, v| v.length == 2 }.map { |k, v| v.map(&:to_i).inject(:*) }.inject(:+)
+end
+
 puts part1(input)
+puts part2(input)
