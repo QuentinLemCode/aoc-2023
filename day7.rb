da5af1376b02024b21dea375934432b3b36df576
QuentinLemCode <quentinlemcode@gmail.com>
2023-12-07 09:12:12 +0100

    day7 p2

--- day7.rb
+++ day7.rb
@@ -61,4 +61,62 @@ def hand_rank(hand)
   end
 end
 
+def part2(input)
+  hands = input.split("\n").map { |line| line.split(' ') }.inject(Hash.new(0)) do |hands, line|
+    hands[line[0]] = line[1]
+    hands
+  end
+  hands.sort do |hand1, hand2|
+    rank1, rank2 = hand_rank2(hand1[0]), hand_rank2(hand2[0])
+    if rank1 == rank2
+      cards1 = hand1[0].chars.map { |card| CARD_VALUES_2[card] }
+      cards2 = hand2[0].chars.map { |card| CARD_VALUES_2[card] }
+      sortval = 0
+      cards1.zip(cards2).each do |card1, card2|
+        next if card1 == card2
+        sortval = card1 < card2 ? 1 : -1
+        break
+      end
+      sortval || 0
+    else
+      rank2 <=> rank1
+    end
+  end.reverse.map.with_index do |hand, i|
+    (i+1) * hand[1].to_i
+  end.sum
+end
+
+def hand_rank2(hand)
+  occ = hand.chars.map{ |x| CARD_VALUES_2[x] }.tally
+  joks = occ.delete(1)
+  return 7 if occ.empty?
+
+  counts = occ.values.sort.reverse
+  counts[0] += joks if joks
+  case counts.reverse
+  when [1, 4]
+    6
+  when [2, 3]
+    5
+  when [1, 1, 3]
+    4
+  when [1, 2, 2]
+    3
+  when [1, 1, 1, 2]
+    2
+  when [1, 1, 1, 1, 1]
+    1
+  else
+    7
+  end
+end
+
+
+CARD_VALUES_2 = {
+  'A' => 14, 'K' => 13, 'Q' => 12, 'J' => 1, 'T' => 10,
+  '9' => 9, '8' => 8, '7' => 7, '6' => 6, '5' => 5,
+  '4' => 4, '3' => 3, '2' => 2
+}
+
 puts part1(input)
+puts part2(input)
