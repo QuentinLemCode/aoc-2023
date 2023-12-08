5e5a473dedb08fe6fe284b64ed61d239253bf89a
QuentinLemCode <quentinlemcode@gmail.com>
2023-12-08 10:08:51 +0100

    day8 p2

--- day8.rb
+++ day8.rb
@@ -2,11 +2,16 @@ require_relative './lib.rb'
 
 input = get_input(8)
 example =
-  'LLR
+  'LR
 
-  AAA = (BBB, BBB)
-  BBB = (AAA, ZZZ)
-  ZZZ = (ZZZ, ZZZ)'
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)'
 
 def part1(input)
   lines = input.split("\n")
@@ -38,4 +43,30 @@ def part1(input)
   steps
 end
 
+def part2(input)
+  lines = input.split("\n")
+  instructions = lines.first.chars
+  maps =
+    lines[2..-1]
+      .map { |line| line.split(' = ') }
+      .inject({}) do |acc, (k, v)|
+        acc[k.strip] = v.gsub(/\(|\)/, '').split(',').map(&:strip)
+        acc
+      end
+  found = false
+  positions = maps.find_all { |k, v| k.end_with?('A') }.map(&:first)
+
+  lens =
+    positions.map do |start_p|
+      instructions.cycle.each_with_index do |instruction, i|
+        direction = instruction == 'R' ? 1 : 0
+        start_p = maps[start_p][direction]
+        break i + 1 if start_p.end_with?('Z')
+      end
+    end
+
+    lens.reduce(&:lcm)
+end
+
 puts part1(input)
+puts part2(input)
