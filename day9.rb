e82bbde7f809f4695dc293681bb8515f004d1e73
QuentinLemCode <quentinlemcode@gmail.com>
2023-12-10 09:59:37 +0100

    day9 p2

--- day9.rb
+++ day9.rb
@@ -17,5 +17,16 @@ def part1(input)
   end.sum
 end
 
+def part2(input)
+  datasets = input.split("\n").map{ |x| x.split(' ').map(&:to_i)}
+  datasets.map do |dataset|
+    sequences = [dataset.reverse]
+    while !sequences.last.all? { |x| x === 0}
+      sequences << sequences.last.each_cons(2).map { |a, b| b - a }
+    end
+    sequences.map(&:last).sum
+  end.sum
+end
 
 puts part1(input)
+puts part2(input)
