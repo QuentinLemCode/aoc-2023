dc6c806509974e0704d395418dcefa8a3a9c0acf
QuentinLemCode <quentinlemcode@gmail.com>
2023-12-12 16:51:29 +0100

    day12 p2

--- day12.rb
+++ day12.rb
@@ -63,7 +63,96 @@ def part1(input)
   end
 end
 
+def memoize(result, memo, pos, conditions)
+  memo[pos] ||= {}
+  memo[pos][conditions.length] = result
+end
+
+def count_arrangements(arrangement, pos, conditions, min_length, memo)
+  if memo[pos]&.[](conditions.length).is_a?(Integer)
+    return memo[pos][conditions.length]
+  end
+  if conditions.empty?
+    return 0 if (arrangement[pos..-1]&.index('#') || -1) >= 0
+    return 1
+  end
+  if pos + min_length > arrangement.length
+    return memoize(0, memo, pos, conditions)
+  end
+  if arrangement[pos] == '.'
+    nextpos = arrangement[pos + 1..-1].chars.find_index { |c| c != '.' }
+    result =
+      count_arrangements(
+        arrangement,
+        pos + (nextpos || 0) + 1,
+        conditions,
+        min_length,
+        memo
+      )
+    return(memoize(result, memo, pos, conditions))
+  end
+  if (pos >= arrangement.length)
+    return memoize(1, memo, pos, conditions) if conditions.length === 0
+    return memoize(0, memo, pos, conditions)
+  end
+
+  if arrangement[pos] == '#'
+    if arrangement.length - pos < conditions[0]
+      return memoize(0, memo, pos, conditions)
+    end
+    conditions[0].times do |i|
+      return memoize(0, memo, pos, conditions) if arrangement[pos + i] == '.'
+    end
+    if arrangement[pos + conditions[0]] == '#'
+      return memoize(0, memo, pos, conditions)
+    end
+    result =
+      count_arrangements(
+        arrangement,
+        pos + conditions[0] + 1,
+        conditions[1..-1],
+        min_length - conditions[0] - 1,
+        memo
+      )
+    return(memoize(result, memo, pos, conditions))
+  elsif arrangement[pos] == '?'
+    result =
+      count_arrangements(arrangement, pos + 1, conditions, min_length, memo)
+    if arrangement.length - pos < conditions[0]
+      return memoize(result, memo, pos, conditions)
+    end
+    conditions[0].times do |i|
+      if arrangement[pos + i] == '.'
+        return memoize(result, memo, pos, conditions)
+      end
+    end
+    if arrangement[pos + conditions[0]] == '#'
+      return memoize(result, memo, pos, conditions)
+    end
+    result +=
+      count_arrangements(
+        arrangement,
+        pos + conditions[0] + 1,
+        conditions[1..-1],
+        min_length - conditions[0] - 1,
+        memo
+      )
+    return memoize(result, memo, pos, conditions)
+  end
+end
+
 def part2(input)
+  input.sum do |arrangement, conditions|
+    new_arr = 5.times.inject([]) { |acc| acc << arrangement }.join('?')
+    new_cond = 5.times.inject([]) { |acc| acc << conditions }.flatten
+    count_arrangements(
+      new_arr,
+      0,
+      new_cond,
+      (new_cond.sum + new_cond.length - 1),
+      {}
+    )
+  end
 end
 
 example = <<-INPUT
@@ -76,4 +165,6 @@ example = <<-INPUT
 INPUT
 
 parsed = parse_input(input)
-puts "Part 1: #{part1(parsed)}"
+
+puts part1(parsed)
+puts part2(parsed)
