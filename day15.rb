cee288bedc698323734c05c9e214805dd6bc0fa2
QuentinLemCode <quentinlemcode@gmail.com>
2023-12-15 08:33:01 +0100

    day15 p1

--- day15.rb
+++ day15.rb
@@ -1,20 +1,29 @@
 require_relative './lib.rb'
 
 input = get_input(15)
-example = ""
+example = 'rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'
 
 def part1(input)
-
+  input
+    .map do |seq|
+      seq
+        .chars
+        .inject(0) do |acc, cur|
+          acc += cur.ord
+          acc *= 17
+          acc % 256
+        end
+    end
+    .sum
 end
 
 def part2(input)
-
 end
 
 def parse(input)
-
+  input.chomp.split(',')
 end
 
-parsed = parse(example)
+parsed = parse(input)
 puts part1(parsed)
 puts part2(parsed)
