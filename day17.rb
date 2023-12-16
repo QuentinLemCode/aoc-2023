a77b110f062beb142dd947b3c432299e83b15d2c
QuentinLemCode <quentinlemcode@gmail.com>
2023-12-16 10:59:28 +0100

    prep day17

---
+++ day17.rb
@@ -0,0 +1,16 @@
+require_relative './lib.rb'
+
+input = get_input(17)
+example = ""
+
+def part1(input)
+end
+
+def part2(input)
+end
+
+def parse_input(input)
+end
+
+parsed = parse_input(example)
+puts part1(parsed)
